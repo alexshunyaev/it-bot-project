353b401de4d46eb9adaafdbca78316ca483ed5fe
MikeSafronov42 <mishasaff@gmail.com>
2024-06-28 23:06:54 +0200

    Tomorrow will try to fix sockets again

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -7,15 +7,16 @@
     //    transports: ['websocket'], // Add this line to specify the transport method
     //});
 
-    import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js" 
+    //import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js" 
 
 
     document.addEventListener('DOMContentLoaded', function () {
+        const io = require('socket.io-client');
         const socket = io("https://it-bot.webpubsub.azure.com", {  
             path: "/clients/socketio/hubs/Hub",
             transports: ['websocket'], // Add this line to specify the transport method
         });   
-        
+
         document.getElementById('userInput').addEventListener('keydown', function (event) {
             if (event.key === 'Enter') {
                 event.preventDefault(); // Prevent the default action (adding a new line)
