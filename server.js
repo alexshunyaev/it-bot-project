a65d9f55e89d9a8aa7d05799fe3bdc9755a02b0b
alexshunyaev <334417@niuitmo.ru>
2024-06-28 11:22:47 +0200

    yebus' s soketami

--- server.js
+++ server.js
@@ -4,30 +4,26 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-const http = require('http');  
-require('dotenv').config();
+const http = require('http');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 const { Server } = require('socket.io');
-const { useAzureSocketIO } = require("@azure/web-pubsub-socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'front-end', 'index.html'));
 });
 
-//TODO: Azure works with socket.io, but requires some additional configuration
-//https://learn.microsoft.com/en-us/answers/questions/1638124/flask-socketio-not-working-in-azure-app-service check this out
-//https://learn.microsoft.com/ru-ru/azure/azure-web-pubsub/socketio-quickstart and this
-//https://learn.microsoft.com/ru-ru/azure/azure-web-pubsub/socketio-migrate-from-self-hosted this too
-
-
-//useAzureSocketIO(io, {
+// Uncomment and configure if Azure socket.io is needed
+// const { useAzureSocketIO } = require("@azure/web-pubsub-socket.io");
+// useAzureSocketIO(io, {
 //    hub: "Hub", 
-//    connectionString: process.argv[2]
-//});
+//    connectionString: process.env.AZURE_PUBSUB_CONNECTION_STRING
+// });
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
@@ -43,23 +39,13 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-//app.use(bodyParser.json());
-//app.use(cors());
-
-//app.use((req, res, next) => {
-//    res.header('Access-Control-Allow-Origin', '*');
-//    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//    next();
-//});
-
-// Serve static files from the "public" directory
+// Serve static files from the "front-end" directory
 app.use(express.static(path.join(__dirname, 'front-end')));
-
 
 io.on('connection', async (socket) => {
     console.log('New client connected');
 
-    socket.on('BotRequest', async (prompt) => { //Waiting for the client to send a message
+    socket.on('BotRequest', async (prompt) => { // Waiting for the client to send a message
         try {
             const response = await axios.post('https://api.openai.com/v1/chat/completions', {
                 model: 'gpt-3.5-turbo',
@@ -81,7 +67,7 @@ io.on('connection', async (socket) => {
         }
     });
 
-    //We don't really need this, but it logs when a client disconnects
+    // Logs when a client disconnects
     socket.on('disconnect', () => {  
         console.log('Client disconnected');
     });
