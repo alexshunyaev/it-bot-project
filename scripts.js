d34bc72aa975122b189e7d7b80c65b1abba12740
alexshunyaev <334417@niuitmo.ru>
2024-07-04 12:41:10 +0200

    refactor

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@
     //    transports: ['websocket'], // Add this line to specify the transport method
     //});
 
-    //import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js" 
+    //import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js"
 
     const io = require('socket.io-client');
     
